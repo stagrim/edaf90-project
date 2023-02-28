4e0c54e2a617868d1c12f37fe7a216aa31c95ad6
Esbjörn Stenberg <me@stagrim.com>
2023-02-28 12:27:35 +0100

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    let num = 0
+
+</script>
+
+<h1>{num}</h1>
+<button on:click={() => num -= 1}>Decrease</button>
+<button on:click={() => num = 0}>Reset</button>
+<button on:click={() => num += 1}>Increase</button>
