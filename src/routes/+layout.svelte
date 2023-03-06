ea4f4d7ea76dc112a1fa2f619c6de362097ecd5b
Adi <adi@creson.se>
2023-03-06 15:50:07 +0100

    Removed margin and padding body (header)

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+    import TopAppBar, {
+        Row,
+        Section,
+        Title,
+        AutoAdjust,
+    } from "@smui/top-app-bar";
+
+    let topAppBar: TopAppBar;
+</script>
+
+<TopAppBar class="top" bind:this={topAppBar} variant="standard">
+    <Row>
+        <Section>
+            <Title>Recipes</Title>
+        </Section>
+        <Section align="end" toolbar />
+    </Row>
+</TopAppBar>
+<AutoAdjust {topAppBar}>
+    <slot />
+</AutoAdjust>
+
+<style>
+    /* Hide everything above this component. */
+    :global(#smui-app),
+    :global(body) {
+        margin: 0;
+        padding: 0;
+    }
+    :global(html) {
+        display: block !important;
+        height: auto !important;
+        width: auto !important;
+        position: static !important;
+    }
+</style>
