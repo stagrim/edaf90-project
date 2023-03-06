89e96220bb6af73e99fcbbe82a5de425331b8cae
daraber <da6003be@student.lu.se>
2023-03-06 15:12:16 +0100

    feat: add top bar

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,49 @@
+<script lang="ts">
+    import TopAppBar, {
+        Row,
+        Section,
+        Title,
+        AutoAdjust,
+    } from '@smui/top-app-bar';
+    import IconButton from '@smui/icon-button';
+
+    let topAppBar: TopAppBar;
+</script>
+
+<TopAppBar bind:this={topAppBar} variant="standard">
+    <Row>
+        <Section>
+            <IconButton class="material-icons">menu</IconButton>
+            <Title>Standard</Title>
+        </Section>
+        <Section align="end" toolbar>
+            <IconButton class="material-icons" aria-label="Download"
+            >file_download
+            </IconButton
+            >
+            <IconButton class="material-icons" aria-label="Print this page"
+            >print
+            </IconButton
+            >
+            <IconButton class="material-icons" aria-label="Bookmark this page"
+            >bookmark
+            </IconButton
+            >
+        </Section>
+    </Row>
+</TopAppBar>
+<AutoAdjust {topAppBar}>
+    <h1>Content</h1>
+</AutoAdjust>
+
+<style>
+    /* Hide everything above this component. */
+    :global(#smui-app),
+    :global(body),
+    :global(html) {
+        display: block !important;
+        height: auto !important;
+        width: auto !important;
+        position: static !important;
+    }
+</style>
