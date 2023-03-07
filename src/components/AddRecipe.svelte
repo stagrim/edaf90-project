1c64abffa7e0f76e63786b9db4fb17c4ac1fe5f9
Adi <adi@creson.se>
2023-03-07 18:18:24 +0100

    Changed header sizes to match new smui typography

--- src/components/AddRecipe.svelte
+++ src/components/AddRecipe.svelte
@@ -1,38 +1,48 @@
 <script lang="ts">
     import Dialog, { Title, Content, Actions } from "@smui/dialog";
     import LayoutGrid from "@smui/layout-grid/src/LayoutGrid.svelte";
-    import Textfield from '@smui/textfield';
-    import HelperText from '@smui/textfield/helper-text';
+    import Textfield from "@smui/textfield";
+    import HelperText from "@smui/textfield/helper-text";
     import Cell from "@smui/layout-grid/src/Cell.svelte";
     import FilePicker from "./FilePicker.svelte";
-    import SegmentedButton, { Segment } from '@smui/segmented-button';
-    import { Label } from '@smui/common';
-    import Button from '@smui/button';
+    import SegmentedButton, { Segment } from "@smui/segmented-button";
+    import { Label } from "@smui/common";
+    import Button from "@smui/button";
 
     export let open = true;
 
-    let form: HTMLFormElement
-    let difficulties = [[1, "Easy"], [2, "medium"], [3, "hard"]]
+    let form: HTMLFormElement;
+    let difficulties = [
+        [1, "Easy"],
+        [2, "medium"],
+        [3, "hard"],
+    ];
 
-    let title = "", description = "", ingredients  = "", steps  = "", minutes: string = "", difficulty: [number, string] = difficulties[0], getImage
+    let title = "",
+        description = "",
+        ingredients = "",
+        steps = "",
+        minutes: string = "",
+        difficulty: [number, string] = difficulties[0],
+        getImage;
 
     const submitForm = async (e: SubmitEvent) => {
-        e.preventDefault()
+        e.preventDefault();
 
-        const image = getImage()
+        const image = getImage();
         if (!image) {
-            return
+            return;
         }
 
         if (!difficulty) {
-            return
+            return;
         }
 
-        fetch("/api/recipe", { 
+        fetch("/api/recipe", {
             method: "POST",
             headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
+                Accept: "application/json",
+                "Content-Type": "application/json",
             },
             body: JSON.stringify({
                 title,
@@ -42,52 +52,92 @@
                 minutes,
                 difficulty: difficulty[0],
                 image,
-            })
-        })
-    }
+            }),
+        });
+    };
 </script>
 
 <Dialog
     bind:open
     aria-labelledby="large-scroll-title"
     aria-describedby="large-scroll-content"
-    surface$style="width: 1300px; max-width: calc(100vw - 32px);">
+    surface$style="width: 1300px; max-width: calc(100vw - 32px);"
+>
     <LayoutGrid style="width: 95%; padding-top: 60px">
         <Cell span={12}>
-            <h1>Add recipe</h1>
+            <h3>Add recipe</h3>
         </Cell>
         <Cell span={6} class="add-form">
-            <Textfield variant="outlined" bind:value={title} label="Title" required>
-                <HelperText slot="helper"></HelperText>
+            <Textfield
+                variant="outlined"
+                bind:value={title}
+                label="Title"
+                required
+            >
+                <HelperText slot="helper" />
             </Textfield>
 
-            <Textfield textarea bind:value={description} style="width: 100%; height: 100px" label="Description" required>
-                <HelperText slot="helper"></HelperText>
-            </Textfield>
-            
-            <Textfield textarea bind:value={ingredients} style="width: 100%; height: 200px" helperLine$style="width: 100%;" label="Ingredients" required>
-                <HelperText slot="helper">Type each ingredient on a different line</HelperText>
-            </Textfield>
-
-            <Textfield textarea bind:value={steps} style="width: 100%; height: 200px;" helperLine$style="width: 100%;" label="Steps" required>
-                <HelperText slot="helper">Type each step on a different line</HelperText>
+            <Textfield
+                textarea
+                bind:value={description}
+                style="width: 100%; height: 100px"
+                label="Description"
+                required
+            >
+                <HelperText slot="helper" />
             </Textfield>
 
+            <Textfield
+                textarea
+                bind:value={ingredients}
+                style="width: 100%; height: 200px"
+                helperLine$style="width: 100%;"
+                label="Ingredients"
+                required
+            >
+                <HelperText slot="helper"
+                    >Type each ingredient on a different line</HelperText
+                >
+            </Textfield>
+
+            <Textfield
+                textarea
+                bind:value={steps}
+                style="width: 100%; height: 200px;"
+                helperLine$style="width: 100%;"
+                label="Steps"
+                required
+            >
+                <HelperText slot="helper"
+                    >Type each step on a different line</HelperText
+                >
+            </Textfield>
         </Cell>
         <Cell span={6}>
-            <h3>Select difficulty</h3>
+            <h5>Select difficulty</h5>
 
-            <Textfield bind:value={minutes} label="Minutes to complete" type="number" required />
+            <Textfield
+                bind:value={minutes}
+                label="Minutes to complete"
+                type="number"
+                required
+            />
 
-            <h3>Select difficulty</h3>
-            <SegmentedButton segments={difficulties} let:segment bind:selected={difficulty} singleSelect required>
+            <h5>Select difficulty</h5>
+            <SegmentedButton
+                segments={difficulties}
+                let:segment
+                bind:selected={difficulty}
+                singleSelect
+                required
+            >
                 <!-- Note: the `segment` property is required! -->
                 <Segment {segment}>
                     <Label>{segment[1]}</Label>
                 </Segment>
             </SegmentedButton>
 
-            <h3>Select image</h3>
+            <h5>Select image</h5>
 
             <FilePicker bind:getImage />
 
--- src/components/RecipeDialog.svelte
+++ src/components/RecipeDialog.svelte
@@ -4,39 +4,38 @@
     import Media from "@smui/card/src/Media.svelte";
     import Card from "@smui/card/src/Card.svelte";
     import Cell from "@smui/layout-grid/src/Cell.svelte";
-    import type {Recipe} from "../utils/types";
+    import type { Recipe } from "../utils/types";
     import StepCheckbox from "./StepCheckbox.svelte";
 
     export let open = false;
     export let recipe: Recipe;
-
 </script>
 
 <Dialog
-        bind:open
-        aria-labelledby="large-scroll-title"
-        aria-describedby="large-scroll-content"
-        surface$style="width: 1300px; max-width: calc(100vw - 32px);"
+    bind:open
+    aria-labelledby="large-scroll-title"
+    aria-describedby="large-scroll-content"
+    surface$style="width: 1300px; max-width: calc(100vw - 32px);"
 >
     <LayoutGrid style="width: 95%; padding-top: 60px">
         <Cell span={6}>
             <Card>
                 <Media
-                        style="background-image: url({recipe.img})"
-                        aspectRatio="16x9"
+                    style="background-image: url({recipe.img})"
+                    aspectRatio="16x9"
                 />
             </Card>
         </Cell>
         <Cell span={6}>
-            <h1 style="margin: 0">
+            <h3 style="margin: 0">
                 {recipe.title}
-            </h1>
+            </h3>
             <p>
                 {recipe.description}
             </p>
         </Cell>
         <Cell span={4}>
-            <h3>Ingredienser</h3>
+            <h5>Ingredienser</h5>
             {#each recipe.ingredients as ingredient, i}
                 <p>
                     {ingredient}
@@ -44,9 +43,9 @@
             {/each}
         </Cell>
         <Cell span={8}>
-            <h3>Tillagning</h3>
+            <h5>Tillagning</h5>
             {#each recipe.steps as instruction, i}
-                <StepCheckbox text="{`${i + 1}. ${instruction}`}"/>
+                <StepCheckbox text={`${i + 1}. ${instruction}`} />
             {/each}
         </Cell>
     </LayoutGrid>
