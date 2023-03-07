8c7a63be923f70ff44e6ac6e2b8acbe50d785ce5
daraber <da6003be@student.lu.se>
2023-03-07 18:05:07 +0100

    feat: add step checkbox

--- src/components/RecipeDialog.svelte
+++ src/components/RecipeDialog.svelte
@@ -1,48 +1,34 @@
 <script lang="ts">
-    import Dialog, { Title, Content, Actions } from "@smui/dialog";
+    import Dialog from "@smui/dialog";
     import LayoutGrid from "@smui/layout-grid/src/LayoutGrid.svelte";
     import Media from "@smui/card/src/Media.svelte";
     import Card from "@smui/card/src/Card.svelte";
     import Cell from "@smui/layout-grid/src/Cell.svelte";
-    import IconButton, { Icon } from "@smui/icon-button";
-    import { Svg } from "@smui/common";
-    import { mdiCloseCircleOutline } from "@mdi/js";
-    import type { Recipe } from "src/utils/types";
+    import type {Recipe} from "../utils/types";
+    import StepCheckbox from "./StepCheckbox.svelte";
+
     export let open = false;
     export let recipe: Recipe;
 
-    /*
-            <Cell span={12}>
-            <IconButton
-                style="float: right"
-                size="normal"
-                on:click={() => (open = false)}
-            >
-                <Icon component={Svg} viewBox="0 0 24 24">
-                    <path d={mdiCloseCircleOutline} />
-                </Icon>
-            </IconButton>
-        </Cell>
-    */
 </script>
 
 <Dialog
-    bind:open
-    aria-labelledby="large-scroll-title"
-    aria-describedby="large-scroll-content"
-    surface$style="width: 1300px; max-width: calc(100vw - 32px);"
+        bind:open
+        aria-labelledby="large-scroll-title"
+        aria-describedby="large-scroll-content"
+        surface$style="width: 1300px; max-width: calc(100vw - 32px);"
 >
     <LayoutGrid style="width: 95%; padding-top: 60px">
         <Cell span={6}>
             <Card>
                 <Media
-                    style="background-image: url({recipe.img})"
-                    aspectRatio="16x9"
+                        style="background-image: url({recipe.img})"
+                        aspectRatio="16x9"
                 />
             </Card>
         </Cell>
         <Cell span={6}>
-            <h1 style="margin: 0%">
+            <h1 style="margin: 0">
                 {recipe.title}
             </h1>
             <p>
@@ -60,9 +46,7 @@
         <Cell span={8}>
             <h3>Tillagning</h3>
             {#each recipe.steps as instruction, i}
-                <p>
-                    {`${i + 1}. ${instruction}`}
-                </p>
+                <StepCheckbox text="{`${i + 1}. ${instruction}`}"/>
             {/each}
         </Cell>
     </LayoutGrid>
---
+++ src/components/StepCheckbox.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+    import Checkbox from '@smui/checkbox';
+    import FormField from '@smui/form-field';
+
+    export let text: string | undefined = undefined;
+</script>
+
+<div>
+    <FormField>
+        <Checkbox/>
+        <p slot="label">{text}</p>
+    </FormField>
+</div>
